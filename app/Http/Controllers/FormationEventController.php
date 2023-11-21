bb8ec2371534f41be10ee6b5ea24851ccad287e3
masepe95 <marcosepe10@gmail.com>
2023-11-21 16:19:02 +0100

    first commit

---
+++ app/Http/Controllers/FormationEventController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FormationEvent;
+use Illuminate\Http\Request;
+
+class FormationEventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FormationEvent $formationEvent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FormationEvent $formationEvent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FormationEvent $formationEvent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FormationEvent $formationEvent)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+    <div class="container py-5">
+        
+        <h1 class="display-5 fw-bold">
+            Benvenuto!
+        </h1>
+
+    </div>
+</div>
+
+@endsection
