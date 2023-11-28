6886287c68e95ec4439e2b155c42e90d6ce3f429
Technology Digitransformer <technology@digitransformer.com>
2023-11-28 14:09:10 +0100

    - Fix Branches Creation

--- app/Filament/Resources/BranchResource/Pages/CreateBranch.php
+++ app/Filament/Resources/BranchResource/Pages/CreateBranch.php
@@ -7,7 +7,9 @@ use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\CFP;
 use Illuminate\Support\Facades\Hash;
+use Mockery\Exception;
 
 class CreateBranch extends CreateRecord
 {
@@ -15,18 +17,24 @@ class CreateBranch extends CreateRecord
 
     protected function handleRecordCreation(array $data): Model
     {
-        $branchUser = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'role_id' => BRANCH,
-        ]);
+        try {
+            $branchUser = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'role_id' => BRANCH,
+            ]);
 
-        unset($data['password']);
-        $data['user_id'] = $branchUser->id;
-        $data['cfp_id'] = auth()->id();
+            unset($data['password']);
+            $data['user_id'] = $branchUser->id;
+            $cfp_id = CFP::where('user_id', '=', auth()->id())->first();
+            $data['cfp_id'] = $cfp_id->id;
 
-        return static::getModel()::create($data);
+            return static::getModel()::create($data);
+        }
+        catch(Exception $e){
+            return('an error occurred: ' . $e->getMessage());
+        }
     }
 
 }
