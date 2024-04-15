72e2aab9e63453d7893cafd28d9603dae8952501
DigiTech <technology@digitransformer.com>
2024-04-15 17:14:36 +0200

    - Finalizing Impersonate users

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -22,48 +22,72 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user, User $model): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user, User $model): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
+    public function delete(User $user, User $model): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model): bool
+    public function restore(User $user, User $model): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model): bool
+    public function forceDelete(User $user, User $model): Response
     {
         //
+//        if (Auth()->user->role_id == ISADMIN) {
+        return $user->role_id == ISADMIN
+            ? Response::allow()
+            : Response::deny('Non puoi visualizzare questo profilo');
     }
 }
