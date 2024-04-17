2250363ca9d939b68f1c459f112863129a1b3c49
DigiTech <technology@digitransformer.com>
2024-04-17 15:28:56 +0200

    - Fix Alumni vs. Camelot status integration;

--- app/Models/Alumnus.php
+++ app/Models/Alumnus.php
@@ -14,8 +14,22 @@ class Alumnus extends Model
         return $this->belongsTo(Branch::class);
     }
 
+    public function camelotCandidate(){
+        return $this->hasOne(CamelotCandidate::class, 'id','camelot_candidate_id');
+    }
+
+    public function camelotCandidateProfile(){
+        return $this->hasOne(CamelotCandidateProfile::class, 'user_id','camelot_candidate_id');
+    }
+
     public function camelotRecruitmentProcessStep()
     {
         return $this->hasOne(CamelotCandidateRecruitmentProcessStep::class, 'id', 'camelot_recruitment_process_step_id');
     }
+
+    public function camelotCandidateStatus() // Va verificata
+    {
+        return $this->hasOneThrough(CamelotCandidateRecruitmentProcessStep::class, CamelotCandidate::class, 'id', 'id', 'camelot_candidate_id','camelot_recruitment_process_step_id');
+    }
+
 }
